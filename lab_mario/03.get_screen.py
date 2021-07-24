6b2cbd1c16e3637174dde61f70a6b731faf12442
EnhasuKim <mj040402@daum.net>
2021-07-24 16:00:07 +0900

    1

---
+++ lab_mario/03.get_screen.py
@@ -0,0 +1,13 @@
+#게임화면 가져오기
+import retro
+
+#게임 환경 생성
+env=retro.make(game='SuperMarioBros-Nes',state='Level1-1')
+#새 게임 시작
+env.reset()
+
+#화면 가져오기
+screen=env.get_screen()
+
+print(screen.shape[0],screen.shape[1])
+print(screen)
---
+++ lab_mario/mario.py
@@ -0,0 +1,5 @@
+import retro
+env = retro.make(game='SuperMarioBros-Nes')
+
+
+#python -m retro.examples.interactive --game SuperMarioBros-Nes
